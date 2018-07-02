96f6d1b4ed3a834618ce869f0c42789f6eb25ab4
Bo Li <ee06b056@users.noreply.github.com>
2018-07-02 19:36:42 -0400

    to project in chapter 08

--- project/mortgage.py
+++ project/mortgage.py
@@ -52,4 +52,13 @@ def compareMortgages(amt, years, fixedRate, pts, ptsRate, varRate1, varRate2, va
     totMonths = years * 12
     fixed1 = Fixed(amt, fixedRate, totMonths)
     fixed2 = FixedWithPts(amt, ptsRate, totMonths, pts)
-# to be continued
+    twoRate = TwoRate(amt, varRate2, totMonths, varRate1, varMonths)
+    morts = [fixed1, fixed2, twoRate]
+    for m in range(totMonths):
+        for mort in morts:
+            mort.makePayment()
+    for m in morts:
+        print(m)
+        print(' Total payment = $' + str(int(m.getTotalPaid())))
+
+compareMortgages(amt = 200000, years = 30, fixedRate = 0.07, pts = 3.25, ptsRate = 0.05, varRate1 = 0.045, varRate2 = 0.095, varMonths = 48)
