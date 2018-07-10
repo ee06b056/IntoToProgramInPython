25a5a5e25e55fed269c9dc234f6c0fec3363e27a
Bo Li <ee06b056@users.noreply.github.com>
2018-07-10 11:46:01 -0400

    assignment00

---
+++ Assignment/ps00.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  9 14:59:49 2018
+
+@author: libo
+
+The assignment 00
+"""
+import math
+import numpy
+
+x = int(input('Please enter the number x:'))
+y = int(input('Please enter the number y:'))
+print(x**y)
+print(math.log2(x))
+print(numpy.log2(x))
---
+++ Assignment/ps01a.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  9 22:20:55 2018
+
+@author: libo
+"""
+
